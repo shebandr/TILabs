a6f980d045d6f66fac6f176c54d214a3edb52efc
Andreyeyeye <shebanoff.andrei@yandex.ru>
2025-02-17 17:57:56 +0700

    мне влом

--- TILabs/Lab1.cs
+++ TILabs/Lab1.cs
@@ -12,7 +12,7 @@ namespace TILabs
 		{
 			Random r = new Random();
 			string result = "";
-			List<string> symbols = new List<string> {"a", "b", "c", "d", "e" };
+			List<string> symbols = new List<string> {"a", "b", "c"};
 			for(int i = 0; i < length; i++)
 			{
 				result += symbols[r.Next(0, symbols.Count)];
@@ -20,5 +20,35 @@ namespace TILabs
 
 			return result;
 		}
+
+		public static string GenString2(int length)
+		{
+
+			Random r = new Random();
+			string result = "";
+			List<string> symbols = new List<string> { "a", "b", "c" };
+			List<double> probabilities = new List<double> { 0.2, 0.5, 0.3 };
+
+			// Генерация строки
+			for (int i = 0; i < length; i++)
+			{
+				double randomValue = r.NextDouble(); // Случайное число от 0 до 1
+				double cumulativeProbability = 0.0;
+
+				for (int j = 0; j < symbols.Count; j++)
+				{
+					cumulativeProbability += probabilities[j];
+					if (randomValue < cumulativeProbability)
+					{
+						result += symbols[j];
+						break;
+					}
+				}
+			}
+
+			return result;
+		}
+
+
 	}
 }
