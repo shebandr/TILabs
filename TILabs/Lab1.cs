ffabb8eda0b1459a617b86c3822f60f27f2dc75f
andreyeyeye <andreyeyeye@DESKTOP-TGB9CN7>
2025-02-18 08:01:26 +0300

    вроде готово

--- TILabs/Lab1.cs
+++ TILabs/Lab1.cs
@@ -8,11 +8,10 @@ namespace TILabs
 {
 	internal class Lab1
 	{
-		public static string GenString1(int length)
+		public static string GenString1(List<string> symbols, int length)
 		{
 			Random r = new Random();
 			string result = "";
-			List<string> symbols = new List<string> {"a", "b", "c"};
 			for(int i = 0; i < length; i++)
 			{
 				result += symbols[r.Next(0, symbols.Count)];
@@ -21,13 +20,12 @@ namespace TILabs
 			return result;
 		}
 
-		public static string GenString2(int length)
+		public static string GenString2(List<string> symbols, List<double> probabilities, int length)
 		{
 
 			Random r = new Random();
 			string result = "";
-			List<string> symbols = new List<string> { "a", "b", "c" };
-			List<double> probabilities = new List<double> { 0.2, 0.5, 0.3 };
+
 
 			// Генерация строки
 			for (int i = 0; i < length; i++)
@@ -49,6 +47,119 @@ namespace TILabs
 			return result;
 		}
 
+        public static List<int> CalcCount(string text)
+        {
+            List<int> counts = new List<int>();
+            List<string> alphabet = new List<string>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (!alphabet.Contains(text[i].ToString()))
+                {
+                    alphabet.Add(text[i].ToString());
+
+                    counts.Add(0);
+                }
+            }
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                for (int q = 0; q < alphabet.Count; q++)
+                {
+                    if (text[i].ToString() == alphabet[q])
+                    {
+                        counts[q]++;
+                        break;
+                    }
+                }
+            }
+            return counts;
+        }
+
+        public static List<int> CalcCountPairs(string text)
+        {
+            List<int> counts = new List<int>();
+            List<string> alphabet = new List<string>();
+
+            for (int i = 0; i < text.Length - 1; i++)
+            {
+                if (!alphabet.Contains(text[i].ToString() + text[i + 1].ToString()))
+                {
+                    alphabet.Add(text[i].ToString() + text[i + 1].ToString());
+
+                    counts.Add(0);
+                }
+            }
+
+            for (int i = 0; i < text.Length - 1; i++)
+            {
+                for (int q = 0; q < alphabet.Count; q++)
+                {
+                    if (text[i].ToString() + text[i + 1].ToString() == alphabet[q])
+                    {
+                        counts[q]++;
+                        break;
+                    }
+                }
+            }
+            for (int i = 0; i < alphabet.Count; i++)
+            {
+                Console.WriteLine(alphabet[i] + " " + counts[i]);
+            }
+            return counts;
+        }
+
+        public static List<int> CalcCountTriples(string text)
+        {
+            List<int> counts = new List<int>();
+            List<string> alphabet = new List<string>();
+
+            for (int i = 0; i < text.Length - 2; i++)
+            {
+                if (!alphabet.Contains(text[i].ToString() + text[i + 1].ToString() + text[i + 2].ToString()))
+                {
+                    alphabet.Add(text[i].ToString() + text[i + 1].ToString() + text[i + 2].ToString());
+
+                    counts.Add(0);
+                }
+            }
+
+            for (int i = 0; i < text.Length - 2; i++)
+            {
+                for (int q = 0; q < alphabet.Count; q++)
+                {
+                    if (text[i].ToString() + text[i + 1].ToString() + text[i + 2].ToString() == alphabet[q])
+                    {
+                        counts[q]++;
+                        break;
+                    }
+                }
+            }
+            for (int i = 0; i < alphabet.Count; i++)
+            {
+                Console.WriteLine(alphabet[i] + " " + counts[i]);
+            }
+            return counts;
+        }
+
+        public static double Shennon(string text, List<int> counts)
+		{
+            double result = 0;
+
+            for (int i = 0; i < counts.Count; i++)
+            {
+                if (counts[i] != 0)
+                {
+                    double p_i = (double)counts[i] / text.Length;
+                    if (p_i > 0)
+                    {
+                        result -= p_i * Math.Log(p_i, 2.0);
+                    }
+                }
+            }
+
+            return result;
+        }
 
 	}
 }
--- TILabs/MainWindow.xaml.cs
+++ TILabs/MainWindow.xaml.cs
@@ -21,7 +21,11 @@ namespace TILabs
 	/// </summary>
 	public partial class MainWindow : Window
 	{
-		public MainWindow()
+
+        List<string> symbols = new List<string> { "a", "b", "c" };
+        List<double> probabilities = new List<double> { 0.2, 0.5, 0.3 };
+		int length = 100;
+        public MainWindow()
 		{
 			InitializeComponent();
 		}
@@ -37,43 +41,102 @@ namespace TILabs
 			{
 				path = saveFileDialog.FileName;
 			}
-			string a = "asfsdagsdgывп";
+			string a = Lab1.GenString1(symbols, length);
 			FileProcessor.StringToFile(path, a);
 		}
 
 		private void FileGen2Lab1_Click(object sender, RoutedEventArgs e)
 		{
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            string path = "";
 
-		}
 
-		private void FileGen3Lab1_Click(object sender, RoutedEventArgs e)
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                path = saveFileDialog.FileName;
+            }
+            string a = Lab1.GenString2(symbols, probabilities, length);
+            FileProcessor.StringToFile(path, a);
+
+        }
+
+		private void Calc1Lab1_Click(object sender, RoutedEventArgs e)
 		{
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            string path = "";
 
-		}
+            if (openFileDialog.ShowDialog() == true)
+            {
+                path = openFileDialog.FileName;
+            }
+            string text = FileProcessor.FileToString(path);
+            OpenFileDialog openFileDialog2 = new OpenFileDialog();
+            string path2 = "";
+
+            if (openFileDialog2.ShowDialog() == true)
+            {
+                path2 = openFileDialog2.FileName;
+            }
+            string text2 = FileProcessor.FileToString(path2);
+            OpenFileDialog openFileDialog3 = new OpenFileDialog();
+            string path3 = "";
+
+            if (openFileDialog3.ShowDialog() == true)
+            {
+                path3 = openFileDialog3.FileName;
+            }
+            string text3 = FileProcessor.FileToString(path3);
 
 
 
-		private void FileRead1Lab1_Click(object sender, RoutedEventArgs e)
-		{
-			OpenFileDialog openFileDialog = new OpenFileDialog();
-			string path = "";
+            List<int> counts11 = Lab1.CalcCount(text);
+            List<int> counts12 = Lab1.CalcCount(text2);
+            List<int> counts13 = Lab1.CalcCount(text3);
 
-			if (openFileDialog.ShowDialog() == true)
-			{
-				path = openFileDialog.FileName;
-			}
-			FileProcessor.FileToString(path);
-		}
+            List<int> counts21 = Lab1.CalcCountPairs(text);
+            List<int> counts22 = Lab1.CalcCountPairs(text2);
+            List<int> counts23 = Lab1.CalcCountPairs(text3);
 
-		private void FileRead2Lab1_Click(object sender, RoutedEventArgs e)
-		{
+            List<int> counts31 = Lab1.CalcCountTriples(text);
+            List<int> counts32 = Lab1.CalcCountTriples(text2);
+            List<int> counts33 = Lab1.CalcCountTriples(text3);
 
-		}
+            double sh11 = Lab1.Shennon(text, counts11);
+            double sh12 = Lab1.Shennon(text2, counts12);
+            double sh13 = Lab1.Shennon(text3, counts13);
 
-		private void FileRead3Lab1_Click(object sender, RoutedEventArgs e)
-		{
+            double sh21 = Lab1.Shennon(text, counts21);
+            double sh22 = Lab1.Shennon(text2, counts22);
+            double sh23 = Lab1.Shennon(text3, counts23);
 
-		}
+            double sh31 = Lab1.Shennon(text, counts31);
+            double sh32 = Lab1.Shennon(text2, counts32);
+            double sh33 = Lab1.Shennon(text3, counts33);
+
+            h1_1.Content = sh11;
+            h1_2.Content = sh12;
+            h1_3.Content = sh13;
+
+            h2_1.Content = sh21;
+            h2_2.Content = sh22;
+            h2_3.Content = sh23;
+
+            h3_1.Content = sh31;
+            h3_2.Content = sh32;
+            h3_3.Content = sh33;
+
+            h4_1.Content = Math.Log(counts11.Count, 2.0);
+            h4_2.Content = Math.Log(counts12.Count, 2.0);
+            h4_3.Content = Math.Log(counts13.Count, 2.0);
+
+
+
+            List<int> countsPairs = Lab1.CalcCountPairs(text);
+            List<int> countsTriples = Lab1.CalcCountTriples(text);
+
+
+        }
+
 
 		private void lab1Open_Click(object sender, RoutedEventArgs e)
 		{
